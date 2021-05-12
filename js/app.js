26381e7966b1d5896e572ac808a3b012954079ce
TBM85 <tballesterm@gmail.com>
2021-05-12 17:51:21 -0400

    Include an image into the image section

--- js/app.js
+++ js/app.js
@@ -1,2 +1,5 @@
+const imageContainer = document.querySelector('.image');
 
-
+const image = document.createElement('img');
+imageContainer.appendChild(image);
+image.src = "https://cdn.pixabay.com/photo/2019/07/21/19/01/landscape-4353358_1280.jpg";
