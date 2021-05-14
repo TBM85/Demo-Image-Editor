5e976cc9440369b3b272d32f7ce10afb2b588be9
TBM85 <tballesterm@gmail.com>
2021-05-13 23:13:16 -0400

    Range controls work

--- js/app.js
+++ js/app.js
@@ -5,14 +5,15 @@ imageContainer.appendChild(image);
 image.src = "https://cdn.pixabay.com/photo/2019/07/21/19/01/landscape-4353358_1280.jpg";
 
 const controllers = [
-  {name: 'brightness', type: 'range', min: 0, max: 200, value: 100},
-  {name: 'contrast', type: 'range', min: 0, max: 200, value: 100},
-  {name: 'hue', type: 'range', min: 0, max: 200, value: 100},
-  {name: 'saturation', type: 'range', min: 0, max: 200, value: 100},
-  {name: 'lightness', type: 'range', min: 0, max: 200, value: 100},
-  {name: 'greyscale', type: 'range', min: 0, max: 200, value: 100}
+  {name: 'brightness', id: 'brightness', type: 'range', min: 0, max: 200, value: 100},
+  {name: 'contrast', id: 'contrast', type: 'range', min: 0, max: 200, value: 100},
+  {name: 'saturation', id: 'saturate', type: 'range', min: 0, max: 200, value: 100},
+  {name: 'hue', id: 'hue-rotate', type: 'range', min: 0, max: 300, value: 0},
+  {name: 'sepia', id: 'sepia', type: 'range', min: 0, max: 100, value: 0},
+  {name: 'greyscale', id: 'greyscale', type: 'range', min: 0, max: 100, value: 0}
 ];
 
+// Create the image editor structure
 const controllersContainer = document.querySelector('.controllers');
 
 const createController = () => {  
@@ -23,12 +24,12 @@ const createController = () => {
 
     const label = document.createElement('label');
     controllerItem.appendChild(label);
-    label.for = `${controller.name}`;
+    label.for = `${controller.id}`;
     label.innerHTML = `${controller.name}`;
 
     const input = document.createElement('input');
     controllerItem.appendChild(input);
-    input.id = `${controller.name}`;
+    input.id = `${controller.id}`;
     input.type = `${controller.type}`;
     input.min = `${controller.min}`;
     input.max = `${controller.max}`;
@@ -64,14 +65,20 @@ const createController = () => {
 
 createController();
 
+// Updates value changes made to range controls
 const inputControllers = document.querySelectorAll(".controller input");
 
 const updateInputsHandler = () => {
   for (let input of inputControllers) {
     const updateInputValueHandler = () => {
+      if (input.id === "hue-rotate") {
+        image.style.setProperty(`--${input.id}`, input.value + "deg");
+      } else {
+        image.style.setProperty(`--${input.id}`, input.value + "%");
+      }
       input.nextElementSibling.innerHTML = input.value;
     };
-    
+
     input.addEventListener("change", updateInputValueHandler);
   }
 }
